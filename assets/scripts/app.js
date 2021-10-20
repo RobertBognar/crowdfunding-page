c478fc376d6134620fd88c9b7671252f7d5f5c2c
RobertBognar <robertbognar2@gmail.com>
2021-10-20 13:07:09 +0200

    bookmark button fixes

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+//Bookmark Button Active State
+
+const bookmark = document.querySelector('.bookmark');
+
+function bookmarkToggle() {
+    bookmark.classList.toggle('bookmark--active');
+
+    if (bookmark.classList.contains('bookmark--active')) {
+        bookmark.setAttribute('aria-pressed', 'true');
+    } else {
+        bookmark.setAttribute('aria-pressed', 'false');
+    }
+}
+
+bookmark.addEventListener('click', bookmarkToggle);
