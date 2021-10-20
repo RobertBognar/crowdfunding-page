acf12c55aae99277b3d7d2d827e1444d72b0848d
RobertBognar <robertbognar2@gmail.com>
2021-10-20 14:03:52 +0200

    hamburger menu enabled, need to fix links to show up

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,11 @@
-//Bookmark Button Active State
+//Defining Constants & Attributes
 const bookmark = document.querySelector('.bookmark');
+const buttonMenu = document.querySelector('.btn--menu');
+const buttonMenuIcon = document.querySelector('.menu-icon');
+const navigation = document.querySelector('.navigation__list');
+const overlay = document.querySelector('.overlay');
 
+//Bookmark Button Active State
 function bookmarkToggle() {
     bookmark.classList.toggle('bookmark--active');
 
@@ -10,5 +15,30 @@ function bookmarkToggle() {
         bookmark.setAttribute('aria-pressed', 'false');
     }
 }
-
 bookmark.addEventListener('click', bookmarkToggle);
+
+//Overlay Section
+function overlayClose() {
+    if (overlay.classList.contains('overlay--modal')) {
+        closeModal();
+    } else {
+        menuToggle();
+    }
+}
+
+//Mobile Hamburger Menu Toggle
+function menuToggle() {
+    navigation.classList.toggle('navigation__list--hidden');
+    overlay.classList.toggle('overlay--hidden');
+    document.body.classList.toggle('disable-scroll');
+
+    if (navigation.classList.contains('navigation__list--hidden')) {
+        buttonMenuIcon.src = './assets/images/icon-hamburger.svg';
+        buttonMenu.setAttribute('aria-expanded', 'false');
+    } else {
+        buttonMenuIcon.src = './assets/images/icon-close-menu.svg';
+        buttonMenu.setAttribute('aria-expanded', 'true');
+    }
+}
+
+buttonMenu.addEventListener('click', menuToggle);
